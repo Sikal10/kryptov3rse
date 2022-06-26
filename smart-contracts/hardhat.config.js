91bf07da8968b2b162be2b5d96f362f01a9cf8a2
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-26 03:38:26 +0100

    hardhat config using rinkeby testnet and deploy file

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,13 +1,11 @@
-//https://eth-goerli.alchemyapi.io/v2/UK3ELpy2oH30QDYemVnYRDuP0UTzm9Jd
-
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
-    goerli: {
-      url: "https://eth-goerli.alchemyapi.io/v2/UK3ELpy2oH30QDYemVnYRDuP0UTzm9Jd",
-      accounts: ["fcc3c528fb45f35e2d67a2a87e7b456c629e38b1ab46519e65a1a5adc4ed5385"]
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/g9SzRUMK_PtJqgDerMgOukmMDKej5Iwb",
+      accounts: ["4aaa3038d77206cc152d6eaf1a077c90b0192daa3b9ef3e4ca7252deff8e7391"]
     }
   }
 }
