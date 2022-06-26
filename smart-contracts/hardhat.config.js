2eaf22506e25a3522c5ff83c08a6e2a69ad14f06
abiolasikal <sikal.sikal.ss@gmail.com>
2022-06-26 03:08:29 +0100

    hardhat config using goerli testnet and deploy file

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -1,21 +1,13 @@
+//https://eth-goerli.alchemyapi.io/v2/UK3ELpy2oH30QDYemVnYRDuP0UTzm9Jd
+
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
-};
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.alchemyapi.io/v2/UK3ELpy2oH30QDYemVnYRDuP0UTzm9Jd",
+      accounts: ["fcc3c528fb45f35e2d67a2a87e7b456c629e38b1ab46519e65a1a5adc4ed5385"]
+    }
+  }
+}
